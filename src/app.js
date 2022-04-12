5bf371504bc624c08d1a9329a9df7ad55a18171d
StudioPaap <anoukpaap@gmail.com>
2022-04-12 16:49:46 +0200

    huiswerk dinsdag

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+async function fetchCountries(){
+
+    try{
+        const response = await axios.get('https://restcountries.com/v2/all');
+        console.log(response);
+        const countriesList = document.getElementById('countryList');
+        const countryDetail = document.createElement("li");
+        countryDetail.innerHTML =
+            `<li> Dit zijn de talen : ${response.data[0].languages[1].name}</li>`
+        ;
+        countriesList.appendChild(countryDetail);
+    }
+    catch (e){
+        console.log(e);
+        console.log(e.response);
+        if (e.response.status === 500) {
+            errorMessage.textContent = "Er ging iets mis in de server";
+
+        }
+    }
+}
+
+fetchCountries();
+
+
